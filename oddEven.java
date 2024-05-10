88a2a80ff5215823aefec85700f44c2c5b262a76
Kavitha0907 <168117493+Kavitha0907@users.noreply.github.com>
2024-05-10 14:59:55 +0530

    oddEven.java

---
+++ oddEven.java
@@ -0,0 +1,11 @@
+
+
+//User function Template for Java
+class Solution{
+    static String oddEven(int N){
+        if(N%2==0)
+        return "even";
+        return "odd";
+        // code here
+    }
+}
